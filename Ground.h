29ecaad8fb11f8a68a8b30a78ce8af9f054bed92
ひらり 野崎 <250108@jc-21.jp>
2026-06-10 12:16:25 +0900

    カメラについてがまだ書き終わってないよ！やろうね

--- Ground.h
+++ Ground.h
@@ -8,20 +8,17 @@ public:
 	//引数：parent  親オブジェクト（SceneManager）
 	Ground(GameObject* parent);
 	~Ground();
-
 	//初期化
 	void Initialize() override;
-
 	//更新
 	void Update() override;
-
 	//描画
 	void Draw() override;
-
 	//開放
 	void Release() override;
-
+	int GetModelHandele() { return hModel_; }
 private:
 	int hModel_;
+	int camType_;
 };
 
--- Tank.cpp
+++ Tank.cpp
@@ -1,7 +1,17 @@
 #include "Tank.h"
 #include "Engine/Model.h"
 #include "Engine/Input.h"
+#include "Engine/Debug.h"
+#include "Ground.h"
 
+namespace
+{
+	XMVECTOR vFront = { 0, 0, 1, 0 };//タンクの前方向ベクトル
+	const float moveSpeed = 0.1f;
+	enum
+}
+
+//タンクのボディを表すクラス
 Tank::Tank(GameObject* parent)
 	:GameObject(parent, "Tank"), hModel_(-1)
 {
@@ -15,6 +25,23 @@ void Tank::Initialize()
 
 void Tank::Update()
 {
+	/////////////
+	switch (camType_)
+	{
+	case FIXED_CAM:
+		Camera::SetTarget(XMFLOAT3(0, 0, 0));
+		Camera::SetPosition(XMFLOAT3(0, 20, -30));
+		break;
+	case TPS_CAM:
+
+		break;
+	case TPS_CAMROT:
+
+		break;
+	case FPS_CAM:
+
+		break;
+	}
 	//Aで左、Dで右
 	if (Input::IsKey(DIK_A))
 	{
@@ -24,6 +51,35 @@ void Tank::Update()
 	{
 		transform_.rotate_.y += 1.0f;
 	}
+	Debug::Log("Yangle=");
+	Debug::Log(transform_.rotate_.y, true);
+	//wキーを押している間、前に進む
+	if (Input::IsKey(DIK_W))
+	{
+		XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
+		XMMATRIX matRot = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));//Y軸回転行列を作る
+		//XMVector3TransformCoordは、ベクトル行列を変換する関数、
+		XMVECTOR vMove = XMVector3TransformCoord(vFront, matRot);
+		vPos = vPos + moveSpeed * vMove;
+		XMStoreFloat3(&transform_.position_, vPos);
+	}
+
+	//レイキャストして、浮いてたら、地面まで落とす
+	RayCastData data;
+	data.start = transform_.position_;
+	data.start.y = 0.0f;//地面は0より下に掘られて作られている。そうじゃないときはもっと上から！
+	data.dir = { 0, -1, 0 };//真下に例を飛ばす
+	Ground* pGround = (Ground*)FindObject("Ground");//ground
+	int hGroundModel = pGround->GetModelHandele();//ground
+	Model::RayCast(hGroundModel, &data);//
+
+	if (data.hit == true)
+	{
+		transform_.position_.y = - data.dist;
+	}
+
+
+	
 }
 
 void Tank::Draw()
